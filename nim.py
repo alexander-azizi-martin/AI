e6cf2c6a23c2b70e900372f605ab213c880683d1
alexander-azizi-martin <alexander.azizi-martin@rogers.com>
2020-04-28 18:18:16 -0400

    Finished nim

--- nim.py
+++ nim.py
@@ -101,7 +101,7 @@ class NimAI():
         Return the Q-value for the state `state` and the action `action`.
         If no Q-value exists yet in `self.q`, return 0.
         """
-        raise NotImplementedError
+        return self.q.get((tuple(state), action), 0)
 
     def update_q_value(self, state, action, old_q, reward, future_rewards):
         """
@@ -118,7 +118,8 @@ class NimAI():
         `alpha` is the learning rate, and `new value estimate`
         is the sum of the current reward and estimated future rewards.
         """
-        raise NotImplementedError
+        self.q[tuple(state), action] = old_q + self.alpha * \
+            (reward + future_rewards - old_q)
 
     def best_future_reward(self, state):
         """
@@ -130,7 +131,12 @@ class NimAI():
         Q-value in `self.q`. If there are no available actions in
         `state`, return 0.
         """
-        raise NotImplementedError
+        best_reward = 0
+
+        for action in Nim.available_actions(state):
+            best_reward = max(best_reward, self.get_q_value(state, action))
+
+        return best_reward
 
     def choose_action(self, state, epsilon=True):
         """
@@ -147,7 +153,21 @@ class NimAI():
         If multiple actions have the same Q-value, any of those
         options is an acceptable return value.
         """
-        raise NotImplementedError
+        actions = Nim.available_actions(state)
+
+        if epsilon and random.random() <= self.epsilon:
+            return random.choice(list(actions))
+
+        else:
+            choice = actions.pop()
+            best_reward = 0
+            for action in Nim.available_actions(state):
+                reward = self.get_q_value(state, action)
+                if reward > best_reward:
+                    choice = action
+                    best_reward = reward
+        
+        return choice
 
 
 def train(n):
