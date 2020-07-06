b8acf687d86e869e9db154fedb3685d5da9200dc
alexander-azizi-martin <alexander.azizi-martin@rogers.com>
2020-07-05 21:52:30 -0400

    Implemented the load_files, tokenize, compute_idfs, and top_files functions

--- questions.py
+++ questions.py
@@ -1,5 +1,8 @@
 import nltk
 import sys
+import os
+import string
+import math
 
 FILE_MATCHES = 1
 SENTENCE_MATCHES = 1
@@ -8,11 +11,12 @@ SENTENCE_MATCHES = 1
 def main():
 
     # Check command-line arguments
-    if len(sys.argv) != 2:
-        sys.exit("Usage: python questions.py corpus")
+    # if len(sys.argv) != 2:
+    #     sys.exit("Usage: python questions.py corpus")
 
     # Calculate IDF values across files
-    files = load_files(sys.argv[1])
+    #files = load_files(sys.argv[1])
+    files = load_files("corpus")
     file_words = {
         filename: tokenize(files[filename])
         for filename in files
@@ -48,7 +52,14 @@ def load_files(directory):
     Given a directory name, return a dictionary mapping the filename of each
     `.txt` file inside that directory to the file's contents as a string.
     """
-    raise NotImplementedError
+    files = {}
+
+    for document_name in os.listdir(directory):
+        if document_name.endswith(".txt"):
+            with open(os.path.join(directory, document_name), encoding="utf8") as document:
+                files[document_name] = document.read()
+
+    return files
 
 
 def tokenize(document):
@@ -59,7 +70,13 @@ def tokenize(document):
     Process document by coverting all words to lowercase, and removing any
     punctuation or English stopwords.
     """
-    raise NotImplementedError
+    words = []
+
+    for token in nltk.word_tokenize(document):
+        if token not in string.punctuation:
+            words.append(token.lower())
+
+    return words
 
 
 def compute_idfs(documents):
@@ -70,7 +87,19 @@ def compute_idfs(documents):
     Any word that appears in at least one of the documents should be in the
     resulting dictionary.
     """
-    raise NotImplementedError
+    words_count = {}
+
+    # Counts the number of documents a word is in
+    for document_name in documents:
+        words_seen = set()
+        for word in documents[document_name]:
+            # Only adds to the count if the word has not been seen in the document
+            if word not in words_seen:
+                words_count[word] = words_count.get(word, 0) + 1
+                words_seen.add(word)
+
+    # Calculates and returns IDF of each word
+    return {word: math.log(len(documents) / words_count[word]) for word in words_count}
 
 
 def top_files(query, files, idfs, n):
@@ -80,7 +109,26 @@ def top_files(query, files, idfs, n):
     to their IDF values), return a list of the filenames of the the `n` top
     files that match the query, ranked according to tf-idf.
     """
-    raise NotImplementedError
+    documents_score = {}
+
+    # Calculates each document score
+    for document in files:
+        documents_score[document] = 0
+        for word in query:
+            term_frequency = files[document].count(word)
+            if term_frequency > 0:
+                documents_score[document] += idfs[word] * term_frequency
+    
+    # Gets the top n documents
+    top_documents = []
+    for (document, score) in sorted(documents_score.items(), key=lambda item: item[1], reverse=True):
+        if n > 0:
+            top_documents.append(document)
+            n -= 1
+        else:
+            break
+    
+    return top_documents
 
 
 def top_sentences(query, sentences, idfs, n):
@@ -91,7 +139,44 @@ def top_sentences(query, sentences, idfs, n):
     the query, ranked according to idf. If there are ties, preference should
     be given to sentences that have a higher query term density.
     """
-    raise NotImplementedError
+    sentences_score = {}
+
+    # Calculates each sentence score
+    for sentence in sentences:
+        sentences_score[sentence] = 0
+        for word in query :
+            if word in sentences[sentence]:
+                sentences_score[sentence] += idfs[word]
+            
+    # Gets the top n sentence
+    top_sentences = []
+    sorted_sentences = sorted(sentences_score.items(), key=lambda item: item[1], reverse=True)
+    for i in range(n):
+        top_sentence, next_sentence = sorted_sentences[i], None
+        if i < len(sorted_sentences) - 1: 
+            next_sentence = sorted_sentences[i + 1]
+
+        while top_sentence[1] == next_sentence[1]:
+            query_term_density_top, query_term_density_next = 0, 0
+
+            for word in query:
+                if word in top_sentence[0]:
+                    query_term_density_top += (1 / len(sentences[top_sentence[0]]))
+                if word in next_sentence[0]:
+                    query_term_density_next += (1 / len(sentences[next_sentence[0]]))
+            
+            if query_term_density_top < query_term_density_next:
+                top_sentence = next_sentence
+
+            i += 1
+            if i < len(sorted_sentences) - 1: 
+                next_sentence = sorted_sentences[i + 1]
+            else:
+                break
+
+        top_sentences.append(top_sentence[0])
+    
+    return top_sentences
 
 
 if __name__ == "__main__":
